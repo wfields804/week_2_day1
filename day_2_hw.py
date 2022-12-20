a34eb55f9f226742b26d544529b256e16b627feb
wfields804 <FieldsBusiness01@gmail.com>
2022-12-20 06:57:22 -0500

    week_2hw

---
+++ day_2_hw.py
@@ -0,0 +1,47 @@
+#Question 1.
+
+cubed = [a**3 for a in range(0,1001) if (a**3)< 1001]
+print(cubed)
+
+
+
+#Question 2
+
+prime = [a/a for a in range(1,101) if (a/1) in range(1,101)]
+print(prime)
+
+#Need help, saw examples online, but do not understand
+
+#Question 3
+
+
+kids = input("Are you under 18?")
+adults = input("Are you 18-65?")
+#Cant figure out how to do 65 and up :(
+if range(1-18):
+    if "yes":
+        print("kids")
+else:
+    range(19-66)
+    if "no":
+        print("adults")
+
+#Cannot get function to run correctly :( only prints adults.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
